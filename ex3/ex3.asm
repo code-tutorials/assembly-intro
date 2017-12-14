10330265188b66e3b5ecb5d7e377aaa3fb1ad220
davy <davy.wybiral@gmail.com>
2017-12-14 14:19:47 -0600

    Fixed comment typo

--- ex3/ex3.asm
+++ ex3/ex3.asm
@@ -2,7 +2,7 @@ global _start
 
 section .text
 _start:
-   mov ecx, 99   ; set ecx to 101
+   mov ecx, 99   ; set ecx to 99
    mov ebx, 42   ; exit status is 42
    mov eax, 1    ; sys_exit system call
    cmp ecx, 100  ; compare ecx to 100
